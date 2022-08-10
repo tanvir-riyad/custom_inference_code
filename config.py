f6691526e1a7ff724c32dab2840c250570547c46
Tanvir Riyad <tanvirriyad12@gmail.com>
2022-08-10 13:33:44 +0200

    add config.py , dataset.py ,utils.py

---
+++ config.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 10:13:02 2022
+
+@author: tanvir
+"""
+
+import torch
+
+
+if torch.cuda.is_available():
+    print("CUDA Available")
+    device = torch.device('cuda:0')
+else:
+    print('CUDA Not Available')
+    device = torch.device('cpu')
+    
+    
+#%% Parameters
+
+batch_size = 6
+img_shape = 256
+epoch = 100
+
+train_dir = 'D:\Plaex\Code\custom-detection\data\train'
+test_dir = 'D:\Plaex\Code\custom-detection\data\test'
+
+
+classes = ['background', 'organic', 'plastic_bottle', 'paper', 'pmd']
+
+num_class = 5
+
+result_dir = 'D:\Plaex\Code\custom-detection\output'
+
+save_plot = 2
+
+
+
---
+++ utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 10:46:26 2022
+
+@author: tanvi
+"""
+
+
+#%%get average for each epoch
+class Averager:
+    
+    def __init__(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+        
+    def send(self, value):
+        self.current_total += value
+        self.iterations += 1
+    
+    @property
+    def value(self):
+        if self.iterations == 0:
+            return 0
+        else:
+            return 1.0 * self.current_total / self.iterations
+    
+    def reset(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+        
+#%%
+def collate_fn(batch):
+    """
+    To handle the data loading as different images may have different number 
+    of objects and to handle varying size tensors as well.
+    """
+    return tuple(zip(*batch))
+
+
